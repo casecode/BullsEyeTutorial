999777495034cd7c33039f7f12087e491e7b7a08
Casey White <casey.r.white@gmail.com>
2014-09-29 15:23:27 -0700

    completed section 02 - Slider and Variables

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
 
+        let alert = UIAlertController(title: "Hello, World",
+                                    message: message,
+                             preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
 }
 
