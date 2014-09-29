8fe32c61578136da62c571f7a2b6d49b84bd3c1f
Casey White <casey.r.white@gmail.com>
2014-09-29 16:20:11 -0700

    completed section 03 - Outlets

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 50
+    var targetValue: Int = 0
+    var currentValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +31,7 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         let message = "The value of the slider is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
 
         let alert = UIAlertController(title: "Hello, World",
                                     message: message,
@@ -34,10 +42,23 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        updateLabels()
     }
 
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
     
 }
